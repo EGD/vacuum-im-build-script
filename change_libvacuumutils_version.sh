063a0859c8747612777b2c487d1a18d305fb8b1a
Andrey Karepin <egdfree@opensuse.org>
2014-04-26 11:03:11 +0400

    Specify variable name.

--- change_libvacuumutils_version.sh
+++ change_libvacuumutils_version.sh
@@ -2,12 +2,12 @@
 
 [[ $# -eq 0 ]] && exit 1
 
-path=~/Документы/projects/Vacuum-IM/home\:EGDFree\:vacuum-im\:git/
+VACUUM_DIR=~/Документы/projects/Vacuum-IM/home\:EGDFree\:vacuum-im\:git/
 
-for plugin in $(ls $path | grep vacuum-im-plugins)
+for plugin in $(ls $VACUUM_DIR | grep vacuum-im-plugins)
 do
-  sed -i "s/libvacuumutils[[:digit:]]*/libvacuumutils$1/" $path/$plugin/$plugin.spec
-  osc commit -m "change version libvacuumutils to $1" $path/$plugin
+  sed -i "s/libvacuumutils[[:digit:]]*/libvacuumutils$1/" $VACUUM_DIR/$plugin/$plugin.spec
+  osc commit -m "change version libvacuumutils to $1" $VACUUM_DIR/$plugin
 done
 
 exit 0
